c422750677fdc4f0e4c578ab019ff78e487bcb48
SebastiánAguilarr <sebastian.aguilargutierrez@gmail.com>
2026-01-28 19:24:03 +0100

    Navbar: reservas siempre al home

--- components/navbar.js
+++ components/navbar.js
@@ -425,7 +425,7 @@ class CustomNavbar extends HTMLElement {
         <div class="nav-links">
           <a href="index.html" data-i18n="navbar.home">Inicio</a>
           <a href="menu.html" data-i18n="navbar.menu">Menú</a>
-          <a href="#reservas" data-i18n="navbar.reservations">Reservas</a>
+          <a href="index.html#reservas" data-i18n="navbar.reservations">Reservas</a>
           <a href="events.html" data-i18n="navbar.events">Eventos</a>
           <a href="index.html#contacto" data-i18n="navbar.contact">Contacto</a>
           
